e26b5028e0ad20fba4672b3b466610246474061d
luu thao <thaon5908@gmail.com>
2018-10-02 15:18:02 +0700

    Them ghi chu

--- Program.cs
+++ Program.cs
@@ -16,6 +16,12 @@ namespace Project
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            // cap nhat
         }
+
+        void init()
+
+    {}
     }
 }
